fefec210f7583167304eb1a41ba1c3d56d2fd5c7
Adrian Bienias <adrianbienias@gmail.com>
2022-10-23 17:08:44 +0200

    Handle SQLite DB with Prisma

--- server.js
+++ server.js
@@ -1,45 +1,55 @@
 // Require the framework and instantiate it
-const fastify = require("fastify")();
-const path = require("path");
-const sha3 = require("crypto-js/sha3");
+const fastify = require("fastify")()
+const path = require("path")
+const sha3 = require("crypto-js/sha3")
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
 
 fastify.register(require("@fastify/static"), {
   root: path.join(__dirname, "public"),
   prefix: "/public/", // optional: default '/'
-});
+})
 
 fastify.get("/rejestracja", (request, reply) => {
-  reply.sendFile("rejestracja.html");
-});
+  reply.sendFile("rejestracja.html")
+})
 
-fastify.register(require("fastify-formidable"));
+fastify.register(require("fastify-formidable"))
 
 fastify.post("/api/user", async (request, reply) => {
-  await request.parseMultipart();
+  await request.parseMultipart()
 
-  let { login, password } = request.body;
-  login = login.trim();
-  password = password.trim();
+  let { login, password } = request.body
+  login = login.trim()
+  password = password.trim()
 
   if (!login) {
-    return { error: "Missing login" };
+    return { error: "Missing login" }
   }
   if (!password) {
-    return { error: "Missing password" };
+    return { error: "Missing password" }
   }
 
-  const hashedPassword = sha3(password).toString();
+  const hashedPassword = sha3(password).toString()
 
-  return { body: request.body, hashedPassword };
-});
+  try {
+    const user = await prisma.user.create({
+      data: { login, password: hashedPassword },
+    })
+
+    return { success: "User created" }
+  } catch (error) {
+    return { error: "User not created" }
+  }
+})
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: 3000 })
   } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
+    fastify.log.error(err)
+    process.exit(1)
   }
-};
-start();
+}
+start()
